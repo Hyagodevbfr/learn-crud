c24af2dcbbdfebdc6924a3127b0b8aace8027b9f
Hyagodevbfr <hyago.sbrito@gmail.com>
2023-03-25 16:43:10 -0300

    :construction: work in Js
    #

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+const tempClient = {
+    nome: "Hyago",
+    email: "hyago.223@gmail.com",
+    celular: "219123456789",
+    cidade: "Queimados"
+}
+const createClient = (client) => {
+}
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
